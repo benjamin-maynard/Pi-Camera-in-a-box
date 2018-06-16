ba428bbe39d4f1522f4647204171622b2e3b3188
root <383838-github@ea.maynard.io>
2018-06-16 13:37:18 +0000

    Add customisation and authorisation

--- containers/nginx-rtsp/resources/entrypoint.sh
+++ containers/nginx-rtsp/resources/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Set the Camera Name in the HTML Web UI
+sed -i -e 's/CAMERANAME/'"$CAMERA_NAME"'/g' /var/www/html/index.html
+
+# Generate a .htpasswd to secure the camera
+printf "$CAMERA_USERNAME:$(openssl passwd -crypt $CAMERA_PASSWORD)\n" >> /usr/local/nginx/cameraauth.htpasswd
+
 # Start nginx
 /usr/local/nginx/sbin/nginx -g 'daemon off;'
