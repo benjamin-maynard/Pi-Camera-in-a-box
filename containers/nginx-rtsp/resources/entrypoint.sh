88baa76438c3bba4ee845b491e4fb5a3a353ce43
Ben Maynard <383838-github@ea.maynard.io>
2018-06-21 17:00:32 +0100

    Added support for multiple users

--- containers/nginx-rtsp/resources/entrypoint.sh
+++ containers/nginx-rtsp/resources/entrypoint.sh
@@ -3,8 +3,18 @@
 # Set the Camera Name in the HTML Web UI
 sed -i -e 's/CAMERANAME/'"$CAMERA_NAME"'/g' /var/www/html/index.html
 
-# Generate a .htpasswd to secure the camera
-printf "$CAMERA_USERNAME:$(openssl passwd -crypt $CAMERA_PASSWORD)\n" >> /usr/local/nginx/cameraauth.htpasswd
+# Split out the values from AUTHORISED_USERS environment variable into username and password strings
+IFS='!' read -ra ADDR <<< "$AUTHORISED_USERS"
+for i in "${ADDR[@]}"; do
+
+# Split out Username and Password to Variables
+CAMUSERNAME=$(echo "$i" | cut -d: -f1)
+CAMPASSWORD=$(echo "$i" | cut -d: -f2)
+
+Add Variable to .htpasswd
+printf "$CAMUSERNAME:$(openssl passwd -crypt $CAMPASSWORD)\n" >> /usr/local/nginx/cameraauth.htpasswd
+
+done
 
 # Start nginx
 /usr/local/nginx/sbin/nginx -g 'daemon off;'
